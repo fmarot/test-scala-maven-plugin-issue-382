446e49846ea7bd086fc3b55e6c44d8cd90a6222a
francois.marot <francois.marot@olea-medical.com>
2020-01-17 17:35:21 +0100

    first commit

---
+++ aaa-depends-on-logback/src/main/java/com/teamtter/App.java
@@ -0,0 +1,17 @@
+package com.teamtter;
+
+public class App {
+	public static void main(String[] args) {
+
+		doNothing();
+	}
+
+	public static void doNothing() {
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+	}
+}
---
+++ shaded-module/src/main/java/com/teamtter/App.java
@@ -0,0 +1,13 @@
+package com.teamtter;
+
+public class App {
+
+	public static void doNothing() {
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+		System.out.println("App AAA Hello World!");
+	}
+}
